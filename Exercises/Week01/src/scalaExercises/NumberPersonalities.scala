a08acd763481ec79ded0a901a473f1b6550d6c3b
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-01-19 17:59:08 +0000

    added skeleton code for week 2 exercises

--- Exercises/Week01/src/scalaExercises/NumberPersonalities.scala
+++ Exercises/Week01/src/scalaExercises/NumberPersonalities.scala
@@ -8,7 +8,7 @@ object NumberPersonalities {
   val limit = 100
 
   def main(args: Array[String]): Unit = {
-    testIsPrimeAndIsComposite()
+    testIsPrime()
 
   }
 
@@ -26,12 +26,11 @@ object NumberPersonalities {
     }
   }
 
-  def isComposite(n: Int): Boolean = {
-    !isPrime(n)
-  }
 
 
-  def testIsPrimeAndIsComposite(): Unit = {
+
+
+  def testIsPrime(): Unit = {
     val primeList = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
       97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
       211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271)
@@ -45,16 +44,8 @@ object NumberPersonalities {
         println("Failed 1: " + num)
         return
       }
-      if (isComposite(num)) {
-        println("Failed 2: " + num)
-        return
-      }
     }
     for (num <- compositeList) {
-      if (!isComposite(num)) {
-        println("Failed 3: " + num)
-        return
-      }
       if (isPrime(num)) {
         println("Failed 4: " + num)
         return

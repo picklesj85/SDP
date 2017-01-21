95a790cf767c160d9db3ea55618e37232a98aae8
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-01-21 10:50:01 +0000

    edited control unit so there is no "newing up"

--- Exercises/Week02/alarmsystem/src/ControlUnit.java
+++ Exercises/Week02/alarmsystem/src/ControlUnit.java
@@ -1,13 +1,14 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class ControlUnit {
 
-  public void pollSensors() {
-    List<Sensor> sensors = new ArrayList<Sensor>();
-    sensors.add(new FireSensor("Bedroom"));
-    sensors.add(new SmokeSensor("Kitchen"));
+  private List<Sensor> sensors;
 
+  public ControlUnit(List<Sensor> sensors) {
+    this.sensors = sensors;
+  }
+
+  public void pollSensors() {
     for (Sensor sensor : sensors) {
       if (sensor.isTriggered()) {
         System.out.println("A " + sensor.getSensorType() + " sensor was triggered at " + sensor.getLocation());
--- Exercises/Week02/alarmsystem/src/ControlUnitTest.java
+++ Exercises/Week02/alarmsystem/src/ControlUnitTest.java
@@ -1,3 +1,16 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlUnitTest {
 
+    @Test
+    public void testPollSensors() {
+        List<Sensor> sensors = new ArrayList<>();
+        sensors.add(new FireSensor("Kitchen"));
+        sensors.add(new SmokeSensor("Bedroom"));
+        ControlUnit cu = new ControlUnit(sensors);
+        cu.pollSensors();
+    }
 }

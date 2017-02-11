7204688991587a069d7721f0b9bed859200f6806
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-02-11 18:06:45 +0000

    tests for AddInstruction

--- Coursework/cw-one/src/test/scala/AddInstructionTest.scala
+++ Coursework/cw-one/src/test/scala/AddInstructionTest.scala
@@ -1,8 +1,25 @@
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 import main.sml._
 
-class AddInstructionTest extends FunSuite{
+class AddInstructionTest extends FunSuite {
+  val m = new Machine(new Labels, Vector())
+
   test("toString") {
-    println(AddInstruction("L1", 1, 2, 3).toString())
+    assert(AddInstruction("L1", 1, 2, 3).toString() == "L1: add 2 + 3 to 1")
+  }
+
+  test("execute 3 different registers") {
+    m.regs(1) = 5
+    m.regs(2) = 5
+    val add = AddInstruction("L1", 3, 1, 2)
+    add.execute(m)
+    assert(m.regs(3) == 10)
+  }
+
+  test("execute with only one register") {
+    m.regs(0) = 1
+    val add = AddInstruction("L1", 0, 0, 0)
+    add.execute(m)
+    assert(m.regs(0) == 2)
   }
 }

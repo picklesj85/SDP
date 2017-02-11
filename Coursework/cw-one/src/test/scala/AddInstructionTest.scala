38321565a23d5585288d98cb51f1d8ad94339fc1
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-02-11 18:28:59 +0000

    SubInstruction class

--- Coursework/cw-one/src/test/scala/AddInstructionTest.scala
+++ Coursework/cw-one/src/test/scala/AddInstructionTest.scala
@@ -1,14 +1,15 @@
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import main.sml._
 
-class AddInstructionTest extends FunSuite {
+class InstructionTest extends FunSuite {
   val m = new Machine(new Labels, Vector())
 
-  test("toString") {
+  // test AddInstruction
+  test("Add toString") {
     assert(AddInstruction("L1", 1, 2, 3).toString() == "L1: add 2 + 3 to 1")
   }
 
-  test("execute 3 different registers") {
+  test("add execute 3 different registers") {
     m.regs(1) = 5
     m.regs(2) = 5
     val add = AddInstruction("L1", 3, 1, 2)
@@ -16,10 +17,30 @@ class AddInstructionTest extends FunSuite {
     assert(m.regs(3) == 10)
   }
 
-  test("execute with only one register") {
+  test("add execute with only one register") {
     m.regs(0) = 1
     val add = AddInstruction("L1", 0, 0, 0)
     add.execute(m)
     assert(m.regs(0) == 2)
+  }
+
+  // test SubInstruction
+  test("Sub toString") {
+    assert(SubInstruction("L1", 1, 2, 3).toString() == "L1: sub 2 - 3 to 1")
+  }
+
+  test("sub execute 3 different registers") {
+    m.regs(1) = 5
+    m.regs(2) = 3
+    val sub = SubInstruction("L1", 3, 1, 2)
+    sub.execute(m)
+    assert(m.regs(3) == 2)
+  }
+
+  test("sub execute with only one register") {
+    m.regs(0) = 1
+    val sub = SubInstruction("L1", 0, 0, 0)
+    sub.execute(m)
+    assert(m.regs(0) == 0)
   }
 }

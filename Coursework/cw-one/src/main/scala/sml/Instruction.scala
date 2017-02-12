0e9a6fbfd7b20e16ba53e56cf34900cebdb76c51
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-02-12 10:15:12 +0000

    took out unnecessary assignment to val

--- Coursework/cw-one/src/main/scala/sml/Instruction.scala
+++ Coursework/cw-one/src/main/scala/sml/Instruction.scala
@@ -5,9 +5,7 @@ abstract class Instruction(label: String, opcode: String) {
   override def toString(): String = label + ": " + opcode
 
   def execute(m: Machine, result: Int, op1: Int, op2: Int, f: (Int, Int) => Int) {
-    val value1 = m.regs(op1)
-    val value2 = m.regs(op2)
-    m.regs(result) = f(value1, value2)
+    m.regs(result) = f(m.regs(op1), m.regs(op2))
   }
 
   def execute(m: Machine): Unit

177754a9fbd0f9c18e3c7a7d2443012b89d16302
picklesj85 <picklesj85@gmail.com>
2017-02-15 15:20:55 +0000

    added maxAverage

--- Exercises/Week04/src/main/scala/functions/functions.scala
+++ Exercises/Week04/src/main/scala/functions/functions.scala
@@ -1,6 +1,7 @@
 package functions
 
-object Funcs {
+object
+Funcs {
 
   // FUNCTIONAL BASICS:
 
@@ -168,7 +169,10 @@ object Funcs {
     *           length is greater than 0.
     * @return the average value of the largest values in the pairs.
     */
-  def maxAverage(ls: List[(Double, Double)]): Double = ???
+  def maxAverage(ls: List[(Double, Double)]): Double = {
+    val max = map(ls)(x => if (x._1 > x._2) x._1 else x._2)
+    foldLeft(max, 0.0)(_ + _) / foldLeft(max, 0.0)((a, b) => 1.0 + a)
+  }
 
   /**
     * variance takes a List[Double] and calculates the squared distance

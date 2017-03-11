b8513017f4b9e143202dd555c45aa8e49ff2464b
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-03-11 13:54:57 +0000

    Q6

--- Exercises/Week08/src/main/scala/builder/SedanCarBuilder.scala
+++ Exercises/Week08/src/main/scala/builder/SedanCarBuilder.scala
@@ -1,21 +1,40 @@
 package builder
 
-//TODO
 
 object SedanCarBuilder extends CarBuilder {
-  override def buildBodyStyle: Unit = ???
 
-  override def buildPower: Unit = ???
+  val sedanCar = Car("SEDAN")
 
-  override def buildEngine: Unit = ???
+  override def buildBodyStyle: Unit = {
+    sedanCar.setBodyStyle("External dimensions: overall length (inches): 202.9," +
+      "\noverall width (inches): 76.2, overall height (inches): 60.7," +
+      "\nwheelbase (inches): 112.9, front track (inches): 65.3," +
+      "\nrear track (inches): 65.5 and curb to curb turning circle (feet): 39.5")
+  }
 
-  override def buildBreaks: Unit = ???
+  override def buildPower: Unit = {
+    sedanCar.setPower("285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm")
+  }
 
-  override def buildSeats: Unit = ???
+  override def buildEngine: Unit = {
+    sedanCar.setEngine("3.5L Duramax V 6 DOHC")
+  }
 
-  override def buildWindows: Unit = ???
+  override def buildBreaks: Unit = {
+    sedanCar.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution")
+  }
 
-  override def buildFuelType: Unit = ???
+  override def buildSeats: Unit = {
+    sedanCar.setSeats(" Front seat centre armrest.Rear seat centre armrest.Split-folding rear seats")
+  }
 
-  override def getCar: Car = ???
+  override def buildWindows: Unit = {
+    sedanCar.setWindows("Laminated side windows.Fixed rear window with defroster")
+  }
+
+  override def buildFuelType: Unit = {
+    sedanCar.setFuelType(" Diesel 19 MPG city, 29 MPG highway, 23 MPG combined and 437 mi. range")
+  }
+
+  override def getCar: Car = sedanCar
 }
--- Exercises/Week08/src/main/scala/builder/SportsCarBuilder.scala
+++ Exercises/Week08/src/main/scala/builder/SportsCarBuilder.scala
@@ -1,21 +1,42 @@
 package builder
 
-// TODO
 
 object SportsCarBuilder extends CarBuilder {
-  override def buildBodyStyle: Unit = ???
 
-  override def buildPower: Unit = ???
+  val sportsCar = Car("SPORTS")
 
-  override def buildEngine: Unit = ???
+  override def buildBodyStyle: Unit = {
+    sportsCar.setBodyStyle("External dimensions: overall length (inches): 192.3," +
+      "\noverall width (inches): 75.5, overall height (inches): 54.2," +
+      "\nwheelbase (inches): 112.3, front track (inches): 63.7," +
+      "\nrear track (inches): 64.1 and curb to curb turning circle (feet): 37.7")
+  }
 
-  override def buildBreaks: Unit = ???
+  override def buildPower: Unit = {
+    sportsCar.setPower(" 323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm")
+  }
 
-  override def buildSeats: Unit = ???
+  override def buildEngine: Unit = {
+    sportsCar.setEngine("3.6L V 6 DOHC and variable valve timing")
+  }
 
-  override def buildWindows: Unit = ???
+  override def buildBreaks: Unit = {
+    sportsCar.setBreaks(" Four-wheel disc brakes: two ventilated. Electronic brake distribution." +
+      "\nStability control")
+  }
 
-  override def buildFuelType: Unit = ???
+  override def buildSeats: Unit = {
+    sportsCar.setSeats(" Driver sports front seat with one power adjustments manual height," +
+      "\nfront passenger seat sports front seat with one power adjustments")
+  }
 
-  override def getCar: Car = ???
+  override def buildWindows: Unit = {
+    sportsCar.setWindows("Front windows with one-touch on two windows")
+  }
+
+  override def buildFuelType: Unit = {
+    sportsCar.setFuelType("Petrol 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range")
+  }
+
+  override def getCar: Car = sportsCar
 }

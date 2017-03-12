5d27f66db307744522d4dece60feae8ea34e4d0f
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-03-12 16:13:21 +0000

    Q10

--- Exercises/Week08/src/main/scala/bridge/Motoren.scala
+++ Exercises/Week08/src/main/scala/bridge/Motoren.scala
@@ -1,11 +1,17 @@
 package bridge
 
-// TODO
 
 case class Motoren(product: Product, s: String) extends Car(product, s) {
-  override def assemble: Unit = ???
+  override def assemble: Unit = {
+    println("Assembling " + product.productName + " for " + s)
+  }
 
-  override def produceProduct: Unit = ???
+  override def produceProduct: Unit = {
+    product.produce
+    println("Modifying product " + product.productName + " according to " + s)
+  }
 
-  override def printDetails: Unit = ???
+  override def printDetails: Unit = {
+    println(s"Car: $s, Product: " + product.productName)
+  }
 }

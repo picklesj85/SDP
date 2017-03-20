4a08f9aa5c61ef6917b8baf88b6a93f8c4497b76
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-03-20 17:24:36 +0000

    Q1

--- Exercises/Week10/src/main/scala/chain/TextFileHandler.scala
+++ Exercises/Week10/src/main/scala/chain/TextFileHandler.scala
@@ -1,9 +1,25 @@
 package chain
 
 case class TextFileHandler(s: String) extends Handler {
-  override def setHandler(handler: Handler): Unit = ???
 
-  override def process(file: File): Unit = ???
+  private var next = None: Option[Handler]
 
-  override def getHandlerName(): String = ???
+  override def setHandler(handler: Handler): Unit = {
+    next = Some(handler)
+  }
+
+  override def process(file: File): Unit = {
+    if (file.fileType == "text") {
+      println("Process and saving doc file... by " + s)
+    } else {
+      next match {
+        case Some(h) => println(s + " forwards request to " + h.getHandlerName())
+                        h.process(file)
+
+        case None => println("File not supported")
+      }
+    }
+  }
+
+  override def getHandlerName(): String = s
 }

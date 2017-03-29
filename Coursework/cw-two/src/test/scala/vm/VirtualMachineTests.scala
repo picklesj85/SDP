79978cf0e708bd0354888fdf52c9378fdea14052
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-03-29 19:48:24 +0100

    push tests

--- Coursework/cw-two/src/test/scala/vm/VirtualMachineTests.scala
+++ Coursework/cw-two/src/test/scala/vm/VirtualMachineTests.scala
@@ -15,9 +15,16 @@ class VirtualMachineTests extends FunSuite {
     assert(vm1.state == Vector())
   }
 
-  test("Pop underflow exception") {
+  test("Pop a few underflow exception") {
     intercept[MachineUnderflowException] {
       vm.pop()._2.pop()._2.pop()._2.pop()
+    }
+  }
+
+  test("empty stack underflow exception") {
+    intercept[MachineUnderflowException] {
+      val vm1 = new VirtualMachineImpl(Vector())
+      vm1.pop()
     }
   }
 
@@ -31,6 +38,21 @@ class VirtualMachineTests extends FunSuite {
     val vm1 = vm.pop()._2.pop()
     assert(vm1._1 == 4)
     assert(vm1._2.state == Vector(6))
+  }
+
+  test("push one on to stack") {
+    val vm1 = vm.push(8)
+    assert(vm1.state == Vector(2, 4, 6, 8))
+  }
+
+  test("push a few onto stack") {
+    val vm1 = vm.push(1).push(2).push(3)
+    assert(vm1.state == Vector(2, 4, 6, 1, 2, 3))
+  }
+
+  test("push and pop") {
+    val vm1 = vm.pop()._2.push(3).push(4).pop()._2.push(7)
+    assert(vm1.state == Vector(2, 4, 3, 7))
   }
 
 }

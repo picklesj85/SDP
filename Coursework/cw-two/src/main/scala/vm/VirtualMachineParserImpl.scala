bd3d7a276c8ed55697be6d455feff6faf15ea84f
jbjorn01-sdp <jbjorn01@mail.bbk.ac.uk>
2017-03-31 21:04:23 +0100

    additional comments

--- Coursework/cw-two/src/main/scala/vm/VirtualMachineParserImpl.scala
+++ Coursework/cw-two/src/main/scala/vm/VirtualMachineParserImpl.scala
@@ -9,18 +9,31 @@ class VirtualMachineParserImpl extends VirtualMachineParser with ByteCodeValues 
   private val bcp = new ByteCodeParserImpl(ByteCodeFactoryImpl)
 
   private def adapter(iv: Vector[Instruction]): Vector[ByteCode] = iv match {
-      // convert our vector of instructions to a vector of bytes and use a
-      // ByteCodeParser to convert into a corresponding vector of ByteCodes
+      // Convert our vector of instructions to a vector of bytes and use a
+      // ByteCodeParser to convert into the corresponding vector of ByteCodes.
+      //
+      // Instructions with no arguments are represented as Vector.empty[Int] by
+      // VendorProgramParserImpl hence require no special handling here as
+      // Vector(1) + Vector.empty[Int] == Vector(1)
+      //
       case h +: t => bcp.parse(bytecode(h.name) +: h.args.map(_.toByte)) ++ adapter(t)
       case _ => Vector.empty[ByteCode]
   }
 
   def parse(file: String): Vector[ByteCode] = {
-    adapter(vp.parse(file))
+    try {
+      adapter(vp.parse(file))
+    } catch{
+      case e: vendor.InvalidInstructionFormatException => throw new bc.InvalidBytecodeException(e.getMessage)
+    }
   }
 
   def parseString(str: String): Vector[ByteCode] = {
-    adapter(vp.parseString(str))
+    try {
+      adapter(vp.parseString(str))
+    } catch{
+      case e: vendor.InvalidInstructionFormatException => throw new bc.InvalidBytecodeException(e.getMessage)
+    }
   }
 
 }

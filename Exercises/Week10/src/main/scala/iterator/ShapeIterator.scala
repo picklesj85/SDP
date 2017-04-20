e8ca595ca91441abacb769bec280e18d753b68f1
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-04-20 19:03:39 +0100

    Q5

--- Exercises/Week10/src/main/scala/iterator/ShapeIterator.scala
+++ Exercises/Week10/src/main/scala/iterator/ShapeIterator.scala
@@ -4,9 +4,19 @@ import java.util.Iterator
 
 class ShapeIterator(private var shapes: Array[Shape]) extends Iterator[Shape] {
 
-  override def hasNext(): Boolean = ???
+  private var count = 0
 
-  override def next(): Shape = ???
+  override def hasNext(): Boolean =  {
+    count < shapes.length
+  }
 
-  override def remove(): Unit = ???
+  override def next(): Shape = {
+    val next = shapes(count)
+    count += 1
+    next
+  }
+
+  override def remove(): Unit =  {
+    shapes = shapes.take(count - 1) ++ shapes.drop(count)
+  }
 }

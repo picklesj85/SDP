b54c6545f7ae86bacd3219b5da68f59ef151b1d9
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-04-20 17:37:55 +0100

    Q3

--- Exercises/Week10/src/main/scala/interpreter/ExpressionUtils.scala
+++ Exercises/Week10/src/main/scala/interpreter/ExpressionUtils.scala
@@ -2,7 +2,14 @@ package interpreter
 
 object ExpressionUtils {
 
-  def isOperator(s: String): Boolean = ???
+  def isOperator(s: String): Boolean = s match {
+    case "+" | "*" | "-" => true
+    case _ => false
+  }
 
-  def getOperator(s: String, left: Expression, right: Expression): Expression = ???
+  def getOperator(s: String, left: Expression, right: Expression): Expression = s match {
+    case "+" => new Add(left, right)
+    case "*" => new Product(left, right)
+    case "-" => new Subtract(left, right)
+  }
 }

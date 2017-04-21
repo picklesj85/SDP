54b92643984ee35de5fdc68394e8bc4a21f35758
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2017-04-21 17:00:14 +0100

    made changes to Q1 platformFactory

--- Exercises/Week11/src/main/scala/flyweight/PlatformFactory.scala
+++ Exercises/Week11/src/main/scala/flyweight/PlatformFactory.scala
@@ -1,13 +1,19 @@
 package flyweight
 
+import java.util.{HashMap, Map}
 
 object PlatformFactory {
 
-  private var map = Map("JAVA" -> new JavaPlatform, "C" -> new CPlatform, "RUBY" -> new RubyPlatform,
-                        "SCALA" -> new ScalaPlatform)
+  private var map: Map[String, Platform] = new HashMap[String, Platform]()
 
-  def getPlatformInstance(platformType: String): Platform = {
-    map(platformType)
+  def getPlatformInstance(platformType: String): Platform = platformType match {
+    case "JAVA" => if (!map.containsKey("JAVA")) map.put("JAVA", new JavaPlatform)
+                   map.get("JAVA")
+    case "SCALA" => if (!map.containsKey("SCALA")) map.put("SCALA", new ScalaPlatform)
+                    map.get("SCALA")
+    case "C" => if (!map.containsKey("C")) map.put("C", new CPlatform)
+                map.get("C")
+    case "RUBY" => if (!map.containsKey("RUBY")) map.put("RUBY", new RubyPlatform)
+                   map.get("RUBY")
   }
-
 }
